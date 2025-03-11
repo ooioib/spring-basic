bfa6715b59c0a373be52405cb95935568cd71316
rin <krin02@naver.com>
2025-03-11 18:04:02 +0900

    feat : InternalResource 예시

--- src/main/java/org/codenova/spring/controller/IndexController.java
+++ src/main/java/org/codenova/spring/controller/IndexController.java
@@ -1,23 +1,32 @@
 package org.codenova.spring.controller;
 
+
+import org.springframework.stereotype.Component;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
 
 /*
-    RequestMappingHandlerMapping을 쓰게 되면 HttpRequestHandler라는 interface를 토대로 만들면 안됨
-    POJO (Plain Old Java Object) 형태로 Handler를 정의 내리면 됨
+    RequestMappingHandelrMapping을 쓰게 되면 HttpRequestHandler 라는 interface 를 토대로 만들면 안됨.
+    POJO (Plain Old Java Object) 형태로 Handler를 정의 내리면 됨.
  */
 
 @Controller
 public class IndexController {
 
-    @RequestMapping({"/" , "index"})
-    public void indexHandler() {
+    @RequestMapping({"/" , "/index"})
+    public ModelAndView indexHandle() {
         System.out.println("IndexHandler.index");
+        ModelAndView mav = new ModelAndView("index");
+        mav.addObject("msg", "Hello World");
+
+        return mav;
     }
 
     @RequestMapping("/help")
-    public void helpHandler(){
+    public String helpHandle() {  // view 이름만 리턴
         System.out.println("IndexHandler.help");
+    return "help";
     }
 }
+
--- src/main/java/org/codenova/spring/controller/UserController.java
+++ src/main/java/org/codenova/spring/controller/UserController.java
@@ -1,20 +1,26 @@
 package org.codenova.spring.controller;
 
+import org.springframework.stereotype.Component;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 @RequestMapping("/user")
 public class UserController {
 
     @RequestMapping("/join")
-    public void joinHandle(){
-        System.out.println("UserController joinHandle");
+    public ModelAndView joinHandle() {
+        System.out.println("UserController.joinHandle");
+
+        ModelAndView mav = new ModelAndView();
+        mav.setViewName("user/join");
+
+        return mav;
     }
 
     @RequestMapping("/join/proceed")
-    public void joinProceedHandle(){
-        System.out.println("UserController joinProceedHandle");
+    public void joinProceedHandle() {
+        System.out.println("UserController.joinProceedHandle");
     }
-
 }
